fde70051f992b454cec0a8282a63890c5123d565
Jeremy <jeremyfoo@gmail.com>
2010-06-22 19:28:49 +0800

    Updated to Xcode 3.2.3 compatible version of Flurry.

--- Flurry/FlurryAPI.h
+++ Flurry/FlurryAPI.h
@@ -26,11 +26,29 @@
 /*
  log events or errors after session has started
  */
-+ (void)setUserID:(NSString *)userID;
 + (void)logEvent:(NSString *)eventName;
 + (void)logEvent:(NSString *)eventName withParameters:(NSDictionary *)parameters;
 + (void)logError:(NSString *)errorID message:(NSString *)message exception:(NSException *)exception;
 + (void)logError:(NSString *)errorID message:(NSString *)message error:(NSError *)error;
+
+/* 
+ start or end timed events
+ */
++ (void)logEvent:(NSString *)eventName timed:(BOOL)timed;
++ (void)logEvent:(NSString *)eventName withParameters:(NSDictionary *)parameters timed:(BOOL)timed;
++ (void)endTimedEvent:(NSString *)eventName;
+
+/*
+ count page views
+ */
++ (void)countPageViews:(id)target;		// automatically track page view on UINavigationController or UITabBarController
++ (void)countPageView;					// manually increment page view by 1
+
+/*
+ set user info
+ */
++ (void)setUserID:(NSString *)userID;	// user's id in your system
++ (void)setAge:(int)age;				// user's age in years
 
 /*
  optional session settings that can be changed after start session
