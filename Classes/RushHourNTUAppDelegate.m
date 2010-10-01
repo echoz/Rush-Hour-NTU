3987f4fadb226547094607f31c9044ca899ab929
Jeremy <jeremyfoo@gmail.com>
2010-10-02 03:47:00 +0800

    using new edventurous framework for buildling

--- Classes/RushHourNTUAppDelegate.m
+++ Classes/RushHourNTUAppDelegate.m
@@ -13,7 +13,7 @@
 #import "FlurryAPI.h"
 
 #define CACH_FILE @"JONTUBusCore.cache"
-#define FLURRY_API_KEY @""
+#define FLURRY_API_KEY @"YSKA262EYEKHYRTKY4GY"
 
 @implementation RushHourNTUAppDelegate
 
